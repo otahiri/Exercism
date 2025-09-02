7468ecdcf126ebe7143d0eb4b27aad23dc705413
otahiri <chucy0066@gmail.com>
2025-09-02 15:30:42 +0100

    .

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -2,5 +2,11 @@
 
 bool leap_year(int year)
 {
-	
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0 && year  % 400 != 0)
+			return 0;
+		return 1;
+	}		
+	return 0;
 }
---
+++ git_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for dir in $(git status --porcelain | awk '{print $2}' | xargs -n1 dirname | sort -u); do
+    echo "Committing changes in $dir"
+    git add "$dir"
+    git commit -m "$dir"
+done
+
