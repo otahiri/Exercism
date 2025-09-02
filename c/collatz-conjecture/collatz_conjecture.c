bddc86fdc56c3d7c434e102fb6417f3669d97b27
otahiri <chucy0066@gmail.com>
2025-09-02 15:46:12 +0100

    c/collatz-conjecture

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -1,1 +1,17 @@
 #include "collatz_conjecture.h"
+
+int steps(int start)
+{
+	int i = 0;
+	if (start <= 0)
+		return -1;
+	while (start != 1)
+	{
+		if(start % 2 == 0)
+			start /= 2;
+		else
+			start = (start * 3) + 1;
+		i++;
+	}
+	return i;
+}
