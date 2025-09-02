f7b392a94fc30b84183deb4454a7d902da510d23
otahiri <chucy0066@gmail.com>
2025-09-02 22:09:38 +0100

    c/grains

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -2,10 +2,27 @@
 
 uint64_t square(uint8_t index)
 {
-
+	uint64_t num = 1;
+	if (index <= 0 || index > 64)
+		return 0;
+	while (index > 1)
+	{
+		num *= 2;
+		index--;
+	}
+	return num;
 }
 uint64_t total(void)
 {
-	
+	uint64_t num = 1;
+	int power = 64;
+	while (power > 1)
+	{
+		num *= 2;
+		num += 1;
+		power--;
+	}
+	return (num);
 }
+
 
