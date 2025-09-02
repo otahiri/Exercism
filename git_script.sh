3d13885a49b3db262476fdef8b0963e60c769432
otahiri <chucy0066@gmail.com>
2025-09-02 15:37:21 +0100

    .

--- git_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for dir in $(git status --porcelain | awk '{print $2}' | xargs -n1 dirname | sort -u); do
-    echo "Committing changes in $dir"
-    git add "$dir"
-    git commit -m "$dir"
-    git push
-done
-
