539cc70015527b8053b1e47bf8d24430646afac4
otahiri <chucy0066@gmail.com>
2025-09-02 22:39:42 +0100

    c/resistor-color

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,25 @@
 #include "resistor_color.h"
+
+
+resistor_band_t *colors()
+{
+	static resistor_band_t color[] = {
+        BLACK,
+        BROWN,
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        VIOLET,
+        GREY,
+        WHITE
+	};
+	return color;
+}
+	
+
+resistor_band_t color_code(resistor_band_t color)
+{
+	return color;
+}
--- c/resistor-color/resistor_color.h
+++ c/resistor-color/resistor_color.h
@@ -2,7 +2,19 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+	BLACK,
+	BROWN,
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	BLUE,
+	VIOLET,
+	GREY,
+	WHITE
 } resistor_band_t;
+resistor_band_t *colors(void);
+
+resistor_band_t color_code(resistor_band_t color);
 
 #endif
