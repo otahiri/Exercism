a9facbc3ab28d8e88a5be45b632bb2d8db96be03
otahiri <chucy0066@gmail.com>
2025-09-03 21:07:41 +0100

    c/rna-transcription

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -1,1 +1,23 @@
 #include "rna_transcription.h"
+#include <string.h>
+#include <stdlib.h>
+
+char *to_rna(const char *dna)
+{
+	int i = 0;
+	char *res = malloc(sizeof(char) * (strlen(dna) + 1));
+	while (dna[i])
+	{
+		if( dna[i] == 'G')
+			res[i] = 'C';
+		if( dna[i] == 'C')
+			res[i] = 'G';
+		if( dna[i] == 'T')
+			res[i] = 'A';
+		if( dna[i] == 'A')
+			res[i] = 'U';
+		i++;
+	}
+	res[i] = '\0';
+	return res;
+}
