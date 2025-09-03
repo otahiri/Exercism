bf5f496980197008ba394ce4b15b58e4350a7891
otahiri <chucy0066@gmail.com>
2025-09-03 20:27:53 +0100

    c/hamming

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,18 @@
 #include "hamming.h"
+#include <string.h>
+
+
+int compute(const char *lhs, const char *rhs)
+{
+	int i = 0;
+	int count = 0;
+	if (strlen(lhs) != strlen(rhs))
+		return -1;
+	while (lhs[i] && rhs[i])
+	{
+		if (lhs[i] != rhs[i])
+			count++;
+		i++;
+	}
+	return count;
+}
