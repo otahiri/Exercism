fc679bfae99f6c6fafb556cac177a80291d68b4e
otahiri <chucy0066@gmail.com>
2025-09-03 19:51:48 +0100

    c/darts

--- c/darts/darts.c
+++ c/darts/darts.c
@@ -1,1 +1,15 @@
 #include "darts.h"
+
+int score(coordinate_t landing_position)
+{
+	float sqrt_x = landing_position.x * landing_position.x;
+	float sqrt_y = landing_position.y * landing_position.y;
+
+	if (sqrt_x + sqrt_y >= 0 && sqrt_x + sqrt_y <= 1)
+		return 10;
+	if (sqrt_x + sqrt_y > 1 && sqrt_x + sqrt_y <= 25)
+		return 5;
+	if (sqrt_x + sqrt_y > 25 && sqrt_x + sqrt_y <= 100)
+		return 1;
+	return 0;
+}
