f7ea89344cafe1bdaa1cb78c7a305cfcc0e67a9f
otahiri <chucy0066@gmail.com>
2025-09-04 15:25:14 +0100

    c/binary

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -1,1 +1,21 @@
 #include "binary.h"
+
+int convert(const char *input)
+{
+	int i = 0;
+	int num = 0;
+	while (input[i])
+	{
+		if (input[i] != '1' && input[i] != '0')
+			return INVALID;
+		i++;
+	}
+	i = 0;
+	while (input[i])
+	{
+		num *= 2;
+		num += (input[i] - '0');
+		i++;
+	}
+	return num;
+}
