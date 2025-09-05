f7b6820d01b8634802b7586c3f79bbf17f43a14b
otahiri <chucy0066@gmail.com>
2025-09-05 20:55:00 +0100

    c/two-fer

--- c/two-fer/two_fer.c
+++ c/two-fer/two_fer.c
@@ -1,1 +1,33 @@
 #include "two_fer.h"
+
+void two_fer(char *buffer, const char *name)
+{
+	int i = 0;
+	int j = 0;
+	char *defult = "you";
+	char *strp1 = "One for ";
+	char *strp2 = ", one for me.";
+	if (name == NULL)
+		name = defult;
+	while (strp1[j])
+	{
+		buffer[i] = strp1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (name[j])
+	{
+		buffer[i] = name[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (strp2[j])
+	{
+		buffer[i] =strp2[j];
+		i++;
+		j++;
+	}
+	buffer[i] = '\0';
+}
