80e5e0c66c51a98a9ec807573cca70d34813cb42
otahiri <chucy0066@gmail.com>
2025-09-10 23:17:14 +0100

    hi

--- c/gigasecond/gigasecond.c
+++ c/gigasecond/gigasecond.c
@@ -1,1 +1,25 @@
 #include "gigasecond.h"
+#include <stdlib.h>  
+#include <stdio.h>  
+
+#define GIGA 1000000000
+#define BUFFER_SIZE sizeof("YYYY-mm-dd HH:MM:SS")
+void fill_arr(int date, int idx, char *arr)
+{
+	if (date == 0)
+		return;
+	fill_arr((date / 10),idx , arr);
+	arr[idx++] = date % 10;
+}
+void gigasecond(time_t input, char *output, size_t size)
+{
+	int idx = 0;
+	input += GIGA;
+	struct tm *future = localtime(&input);
+	fill_arr(future->tm_year + 1900, idx, output )
+	idx += 4;
+	output[idx] = '-';
+	fill_arr(future->tm_month, idx, output )
+		
+
+}
--- c/gigasecond/test_gigasecond.c
+++ c/gigasecond/test_gigasecond.c
@@ -59,7 +59,6 @@ static void test_date(void)
 
 static void test_another_date(void)
 {
-   TEST_IGNORE();   // delete this line to run test
    time_t before = construct_date(1977, 6, 13, 0, 0, 0);
    char after[BUFFER_SIZE] = { 0 };
    gigasecond(before, after, BUFFER_SIZE);
@@ -68,7 +67,6 @@ static void test_another_date(void)
 
 static void test_third_date(void)
 {
-   TEST_IGNORE();
    time_t before = construct_date(1959, 7, 19, 0, 0, 0);
    char after[BUFFER_SIZE] = { 0 };
    gigasecond(before, after, BUFFER_SIZE);
@@ -77,7 +75,6 @@ static void test_third_date(void)
 
 static void test_date_and_time(void)
 {
-   TEST_IGNORE();
    time_t before = construct_date(2015, 1, 24, 22, 0, 0);
    char after[BUFFER_SIZE] = { 0 };
    gigasecond(before, after, BUFFER_SIZE);
@@ -86,22 +83,19 @@ static void test_date_and_time(void)
 
 static void test_date_and_time_with_day_rollover(void)
 {
-   TEST_IGNORE();
    time_t before = construct_date(2015, 1, 24, 23, 59, 59);
    char after[BUFFER_SIZE] = { 0 };
    gigasecond(before, after, BUFFER_SIZE);
    TEST_ASSERT_EQUAL_STRING("2046-10-03 01:46:39", after);
 }
 
-/*
 static void test_your_birthday(void)
 {
-   time_t birthday = construct_date(1989, 1, 1, 1, 1, 1);
+   time_t birthday = construct_date(2001, 1, 31, 1, 1, 1);
    char gigday[BUFFER_SIZE] = {0};
    gigasecond(birthday, gigday, BUFFER_SIZE);
    printf("%s", gigday);
 }
-*/
 
 int main(void)
 {
