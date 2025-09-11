7cc91b6263bfe3e5e3b71a92c6692f6dd2f81027
otahiri <chucy0066@gmail.com>
2025-09-11 23:59:04 +0100

    c/triangle

--- c/triangle/triangle.c
+++ c/triangle/triangle.c
@@ -1,1 +1,19 @@
 #include "triangle.h"
+
+bool is_valid(triangle_t sides)
+{
+	return (sides.a + sides.b > sides.c && sides.b + sides.c > sides.a && sides.a + sides.c > sides.b);
+}
+
+bool is_equilateral(triangle_t sides)
+{
+	return (is_valid(sides) && sides.a == sides.b && sides.b == sides.c);
+}
+bool is_isosceles(triangle_t sides)
+{
+	return (is_valid(sides) && (sides.a == sides.b || sides.b == sides.c || sides.a == sides.c));
+}
+bool is_scalene(triangle_t sides)
+{
+	return (is_valid(sides) && sides.a != sides.b && sides.b != sides.c && sides.a != sides.c);
+}
