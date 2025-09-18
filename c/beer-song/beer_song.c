3bdb9d5a9b28354b2ab4eafbbb0c7e53059be2f8
otahiri <chucy0066@gmail.com>
2025-09-18 14:57:04 +0100

    beer-song

--- c/beer-song/beer_song.c
+++ c/beer-song/beer_song.c
@@ -1,1 +1,27 @@
 #include "beer_song.h"
+#include <stdint.h>
+#include <stdio.h>
+
+// 50 bottles of beer on the wall, 50 bottles of beer.Take one down and pass it
+// around, 49 bottles of beer on the wall.
+static const char *FIRST_LINE[] = {
+    "No more bottles of beer on the wall, no more bottles of beer.",
+    "1 bottle of beer on the wall, 1 bottle of beer.",
+    "%u bottles of beer on the wall, %u bottles of beer."};
+
+static const char *SECOND_LINE[] = {
+    "Go to the store and buy some more, 99 bottles of beer on the wall.",
+    "Take it down and pass it around, no more bottles of beer on the wall.",
+    "Take one down and pass it around, 1 bottle of beer on the wall.",
+    "Take one down and pass it around, %u bottles of beer on the wall."};
+#define MAX_SIZE (1024)
+
+void recite(uint8_t start_bottles, uint8_t take_down, char **song) {
+  uint8_t end = start_bottles - (take_down - 1);
+  for (uint8_t bottle = start_bottles; bottle >= end; bottle--) {
+    snprintf(*song++, MAX_SIZE, FIRST_LINE[bottle > 1 ? 2 : bottle], bottle,
+             bottle);
+    snprintf(*song++, MAX_SIZE, SECOND_LINE[bottle - 1 > 1 ? 3 : bottle],
+             bottle - 1);
+  }
+}
