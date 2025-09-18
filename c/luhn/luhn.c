0290d3abd0e4abdc30d29a6ec9a60be0606dc6cd
otahiri <chucy0066@gmail.com>
2025-09-18 14:57:05 +0100

    luhn

--- c/luhn/luhn.c
+++ c/luhn/luhn.c
@@ -1,32 +1,27 @@
 #include "luhn.h"
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
-
-bool luhn(const char *num)
-{
-	int sum = 0;
-	int doub = 0;
-	int i = strlen(num) - 1;
-	int len = 0;
-	for (;i >= 0 ; i--)
-	{
-		if ((!isdigit(num[i]) && num[i] != ' '))
-			return false;
-		if (num[i] == ' ')
-			continue;
-		int digit = num[i] - '0';
-		len++;
-		if (doub)
-		{
-			int dbl = digit * 2;
-			sum += dbl % 10 + dbl / 10;
-		}
-		if (!doub)
-		{
-			sum += digit;
-		}
-		doub = !doub;
-	}
-	return !(sum % 10) && (len - 1);
+bool luhn(const char *num) {
+  int sum = 0;
+  int doub = 0;
+  int i = strlen(num) - 1;
+  int len = 0;
+  for (; i >= 0; i--) {
+    if ((!isdigit(num[i]) && num[i] != ' '))
+      return false;
+    if (num[i] == ' ')
+      continue;
+    int digit = num[i] - '0';
+    len++;
+    if (doub) {
+      int dbl = digit * 2;
+      sum += dbl % 10 + dbl / 10;
+    }
+    if (!doub) {
+      sum += digit;
+    }
+    doub = !doub;
+  }
+  return !(sum % 10) && (len - 1);
 }
