933e0201c4a9b1773d26e06879248b3a5fd49ef9
otahiri <chucy0066@gmail.com>
2025-09-18 14:57:05 +0100

    sieve

--- c/sieve/sieve.c
+++ c/sieve/sieve.c
@@ -1,29 +1,25 @@
 #include "sieve.h"
 #include <stdio.h>
 
-int is_divis(uint32_t *arr, uint32_t num, int idx)
-{
-	for (int j = 0; j <= idx; j++)
-	{
-		if (num != arr[j] && !(num % arr[j]))
-			return 1;
-	}
-	return 0;
+int is_divis(uint32_t *arr, uint32_t num, int idx) {
+  for (int j = 0; j <= idx; j++) {
+    if (num != arr[j] && !(num % arr[j]))
+      return 1;
+  }
+  return 0;
 }
 
-uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
-{
-	if (limit < 2)
-		return 0;
-	uint32_t idx = 0;
-	primes[idx++] = 2;
-	for (uint32_t i = 3; i <= limit; i++)
-	{
-		if (is_divis(primes, i, idx - 1))
-			continue;
-		primes[idx++] = i;
-		if (idx == (uint32_t)max_primes)
-			break;
-	}
-	return idx;
+uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes) {
+  if (limit < 2)
+    return 0;
+  uint32_t idx = 0;
+  primes[idx++] = 2;
+  for (uint32_t i = 3; i <= limit; i++) {
+    if (is_divis(primes, i, idx - 1))
+      continue;
+    primes[idx++] = i;
+    if (idx == (uint32_t)max_primes)
+      break;
+  }
+  return idx;
 }
