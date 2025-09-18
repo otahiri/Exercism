62603dbade0d9159963321f3c075d851c696e563
otahiri <chucy0066@gmail.com>
2025-09-18 16:11:21 +0100

    c/prime-factors

--- c/prime-factors/prime_factors.c
+++ c/prime-factors/prime_factors.c
@@ -1,27 +1,36 @@
 #include "prime_factors.h"
+#include <math.h>
 #include <stddef.h>
 #include <stdint.h>
 
-uint64_t next_prime(uint64_t n) {
-  if (n == 2)
-    return 3;
-  uint64_t power = 0;
-  uint64_t remain = n - 1;
-  while ((remain & 1) == 0) {
-    remain >>= 1;
-    power++;
+uint64_t is_prime(uint64_t n) {
+  double fact = sqrt((double)n);
+  for (uint64_t i = 2; i <= fact; i++) {
+    if ((n % i) == 0)
+      return 0;
   }
+  return 1;
+}
+
+uint64_t next_prime(uint64_t n) {
+  if (n < 2)
+    return 2;
+  n = (n % 2 == 0) ? n + 1 : n + 2;
+  while (!is_prime(n))
+    n += 2;
+  return n;
 }
 
 size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS]) {
-  uint64_t prime = 2;
+  uint64_t factor = 2;
   size_t i = 0;
-  while (n > 1) {
-    if (n % prime == 0) {
-      n /= prime;
-      factors[i++] = prime;
-    } else
-      prime = next_prime(prime);
+  while (factor <= n) {
+    if ((n % factor) == 0) {
+      factors[i++] = factor;
+      n /= factor;
+    } else {
+      factor = next_prime(factor);
+    }
   }
   return i;
 }
--- c/prime-factors/prime_factors.h
+++ c/prime-factors/prime_factors.h
@@ -7,5 +7,7 @@
 #define MAXFACTORS 10
 
 size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS]);
+uint64_t is_prime(uint64_t n);
+uint64_t next_prime(uint64_t n);
 
 #endif
