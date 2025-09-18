11c5184eb7b66801d5c949184549ebe7d467f8cc
otahiri <chucy0066@gmail.com>
2025-09-18 16:30:50 +0100

    c/nth-prime

--- c/nth-prime/nth_prime.c
+++ c/nth-prime/nth_prime.c
@@ -1,1 +1,30 @@
 #include "nth_prime.h"
+#include <math.h>
+#include <stdint.h>
+
+int is_prime(uint32_t prime) {
+  double fact = sqrt((double)prime);
+  for (uint32_t i = 2; i <= fact; i++) {
+    if (!(prime % i))
+      return 0;
+  }
+  return 1;
+}
+
+uint32_t next_prime(uint32_t prime) {
+  prime = (prime % 2) == 0 ? prime + 1 : prime + 2;
+  while (!is_prime(prime)) {
+    prime += 2;
+  }
+  return prime;
+}
+
+uint32_t nth(uint32_t n) {
+  if (n == 0)
+    return 0;
+  uint32_t prime = 2;
+  for (; n > 1; n--) {
+    prime = next_prime(prime);
+  }
+  return prime;
+}
--- c/nth-prime/nth_prime.h
+++ c/nth-prime/nth_prime.h
@@ -4,5 +4,7 @@
 #include <stdint.h>
 
 uint32_t nth(uint32_t n);
+int is_prime(uint32_t prime);
+uint32_t next_prime(uint32_t prime);
 
 #endif
