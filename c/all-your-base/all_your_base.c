4978b2c93d2c1484b47eb2b3297df744b114f1da
otahiri <chucy0066@gmail.com>
2025-09-19 19:55:11 +0100

    c/all-your-base

--- c/all-your-base/all_your_base.c
+++ c/all-your-base/all_your_base.c
@@ -1,1 +1,29 @@
 #include "all_your_base.h"
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+
+size_t rebase(int8_t digits[], int16_t input_base, int16_t output_base,
+              size_t input_length) {
+  if (input_base < 2 || output_base < 2 || input_length < 1)
+    goto ERR;
+  int32_t num = 0;
+  for (size_t i = 0; i < input_length; i++) {
+    if (digits[i] < 0 || digits[i] >= input_base)
+      goto ERR;
+    num *= input_base;
+    num += digits[i];
+  }
+  int32_t digit_count = 0;
+  for (; pow(output_base, digit_count + 1) < num; digit_count++)
+    ;
+  size_t output = digit_count + 1;
+  for (size_t i = digit_count; i + 1 > 0; i--) {
+    digits[i] = num % output_base;
+    num /= output_base;
+  }
+  return output;
+ERR:
+  digits[0] = 0;
+  return 0;
+}
--- c/all-your-base/all_your_base.h
+++ c/all-your-base/all_your_base.h
@@ -1,6 +1,10 @@
 #ifndef ALL_YOUR_BASE_H
 #define ALL_YOUR_BASE_H
+#include <stdint.h>
+#include <stdlib.h>
 
 #define DIGITS_ARRAY_SIZE 64
 
+size_t rebase(int8_t digits[], int16_t input_base, int16_t output_base,
+              size_t input_length);
 #endif
