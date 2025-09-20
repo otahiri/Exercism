82a6eee03f23184fd3cc8b85f8e7f755254f35c3
otahiri <chucy0066@gmail.com>
2025-09-20 19:33:03 +0100

    c/sublist

---
+++ c/sublist/sublist.c
@@ -0,0 +1,7 @@
+#include "sublist.h"
+
+comparison_result_t check_lists(int *list_to_compare, int *base_list,
+                                size_t list_to_compare_element_count,
+                                size_t base_list_element_count) {
+  for
+}
