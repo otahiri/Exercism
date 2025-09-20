92f274c755b1882af0eb3b74fbb21948087ca92c
otahiri <chucy0066@gmail.com>
2025-09-20 19:18:53 +0100

    c/scrabble-score

--- c/scrabble-score/scrabble_score.c
+++ c/scrabble-score/scrabble_score.c
@@ -1,5 +1,22 @@
 #include "scrabble_score.h"
+#include <ctype.h>
+
+int is_in_str(char *str, char c) {
+  for (int i = 0; str[i]; i++) {
+    if (str[i] == toupper(c))
+      return 1;
+  }
+  return 0;
+}
 
 unsigned int score(const char *word) {
-  char **scores = {}
+  char *chars[7] = {"AEIOULNRST", "DG", "BCMP", "FHVWY", "K", "JX", "QZ"};
+  int scores[7] = {1, 2, 3, 4, 5, 8, 10};
+  int res = 0;
+  for (int i = 0; word[i]; i++) {
+    for (int j = 0; j < 7; j++)
+      if (is_in_str(chars[j], word[i]))
+        res += scores[j];
+  }
+  return res;
 }
--- c/scrabble-score/scrabble_score.h
+++ c/scrabble-score/scrabble_score.h
@@ -2,5 +2,6 @@
 #define SCRABBLE_SCORE_H
 
 unsigned int score(const char *word);
+int is_in_str(char *str, char c);
 
 #endif
