dba7558d0c91b218830b6a393842fcb23d7f5563
otahiri <chucy0066@gmail.com>
2025-09-22 22:26:39 +0100

    lk

--- c/variable-length-quantity/variable_length_quantity.c
+++ c/variable-length-quantity/variable_length_quantity.c
@@ -1,21 +1,33 @@
 #include "variable_length_quantity.h"
+#include <stddef.h>
 #include <stdint.h>
 
-void encode_int(uint32_t integer, uint8_t *res) {
+int encode_int(uint32_t integer, uint8_t *res) {
   int i = 0;
   while (integer > 0) {
     res[i++] = integer & 127;
     integer /= 127;
   }
   res[i - 1] += 128;
+  return i;
 }
 
 int encode(const uint32_t *integers, size_t integers_len, uint8_t *output) {
-  uint8_t buffer[] for (int i = 0; i < integers_len; i++) {}
+   uint8_t buffer[5];
+  int size;
+  int len = 0;
+  for (size_t i = 0; i < integers_len; i++) {
+   size = encode_int(integers[i], buffer);
+    for (int j = size - 1; j >= 0; j--)
+      output[len++] = buffer[j];
+  }
+  return len;;
 }
 
 int decode(const uint8_t *bytes, size_t buffer_len, uint32_t *output) {
   // write to `output`, return final output's length
   // return -1 if error
   // `output` buffer should be enough to hold the full result
+
+  return 0;
 }
