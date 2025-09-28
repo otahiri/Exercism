9fa6624db2b27bfd174a71a6896ebc42d0c0cf4a
otahiri <chucy0066@gmail.com>
2025-09-28 19:12:23 +0100

    c/circular-buffer

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,1 +1,16 @@
 #include "circular_buffer.h"
+
+circular_buffer_t new_circular_buffer(capacity)
+{
+  circular_buffer_t circ;
+  circ = calloc(1, sizeof(circular_buffer_t));
+  circ.buffer = calloc(capacity, sizeof(buffer_value_t));
+  circ.capacity = capacity;
+  return circ;
+}
+
+void delete_buffer(circular_buffer_t buffer)
+{
+  free(buffer.buffer);
+  free(buffer);
+}
--- c/circular-buffer/circular_buffer.h
+++ c/circular-buffer/circular_buffer.h
@@ -1,4 +1,19 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+
+typedef uint16_t buffer_value_t;
+typedef struct{
+  buffer_value_t *buffer;
+  size_t head;
+  size_t tail;
+  size_t size;
+  size_t capacity;
+}circular_buffer_t;
+
+circular_buffer_t new_circular_buffer(capacity);
+void delete_buffer(circular_buffer_t buffer);
 
 #endif
