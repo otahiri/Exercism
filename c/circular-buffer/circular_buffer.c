799e3b1c5859c5abe2424d5ffbbb4a17c61ddb85
otahiri <chucy0066@gmail.com>
2025-09-29 14:35:30 +0100

    added : circular buffer

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,16 +1,73 @@
 #include "circular_buffer.h"
 
-circular_buffer_t new_circular_buffer(capacity)
+circular_buffer_t *new_circular_buffer(size_t capacity)
 {
-  circular_buffer_t circ;
+  if (!capacity)
+    return NULL;
+  circular_buffer_t *circ;
   circ = calloc(1, sizeof(circular_buffer_t));
-  circ.buffer = calloc(capacity, sizeof(buffer_value_t));
-  circ.capacity = capacity;
+  if (!circ)
+    return NULL;
+  circ->buffer = calloc(capacity, sizeof(buffer_value_t));
+  if (!circ->buffer)
+  {
+    free(circ);
+    return NULL;
+  }
+  circ->capacity = capacity;
   return circ;
 }
 
-void delete_buffer(circular_buffer_t buffer)
+void delete_buffer(circular_buffer_t *buffer)
 {
-  free(buffer.buffer);
+  free(buffer->buffer);
   free(buffer);
+}
+
+int16_t write(circular_buffer_t *buffer, buffer_value_t value)
+{
+  if (buffer->size == buffer->capacity)
+  {   
+    errno = ENOBUFS;
+    return EXIT_FAILURE;
+  }
+  buffer->buffer[buffer->head] = value;
+  buffer->head = (buffer->head + 1) % buffer->capacity;
+  buffer->size++;
+  return EXIT_SUCCESS;
+}
+
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value)
+{
+  if (buffer->size == buffer->capacity)
+  {
+    buffer->buffer[buffer->head] = value;
+    buffer->tail = (buffer->tail + 1) % buffer->capacity;
+    buffer->head = (buffer->head + 1) % buffer->capacity;
+    return EXIT_SUCCESS;
+  }
+  buffer->buffer[buffer->head] = value;
+  buffer->head = (buffer->head + 1) % buffer->capacity;
+  buffer->size++;
+  return EXIT_SUCCESS;
+}
+
+int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value)
+{
+  if (!buffer->size)
+  {
+    errno = ENODATA;
+    return EXIT_FAILURE;
+  }
+  *read_value = buffer->buffer[buffer->tail];
+  buffer->tail = (buffer->tail + 1) % buffer->capacity;
+  buffer->size--;
+  return EXIT_SUCCESS;
+}
+
+void clear_buffer(circular_buffer_t *buffer)
+{
+  buffer->size = 0;
+  buffer->head = 0;
+  buffer->tail = 0;
 }
--- c/circular-buffer/circular_buffer.h
+++ c/circular-buffer/circular_buffer.h
@@ -1,5 +1,6 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -13,7 +14,11 @@ typedef struct{
   size_t capacity;
 }circular_buffer_t;
 
-circular_buffer_t new_circular_buffer(capacity);
-void delete_buffer(circular_buffer_t buffer);
+circular_buffer_t *new_circular_buffer(size_t capacity);
+void delete_buffer(circular_buffer_t *buffer);
+int16_t write(circular_buffer_t *buffer, buffer_value_t value);
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value);
+int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value);
+void clear_buffer(circular_buffer_t *buffer);
 
 #endif
