8fe994453ba28ef94f49ca971c9618f07bfeb0de
otahiri <chucy0066@gmail.com>
2025-10-02 22:31:50 +0100

    c/saddle-points

--- c/saddle-points/saddle_points.c
+++ c/saddle-points/saddle_points.c
@@ -1,1 +1,26 @@
 #include "saddle_points.h"
+
+saddle_points_t *saddle_points(size_t width, size_t height, uint8_t **matrix)
+{
+  size_t idx = 0;
+  size_t max_points = (width < height) ? width : height;
+  saddle_points_t *points = malloc(sizeof(saddle_points_t *) * max_points);
+  points[idx]->x = 0;
+  points[idx]->y = 0;
+  for (size_t i = 0; i < height; i++)
+  {
+    for (size_t j = 0; j < width; j++)
+    {
+      if (matrix[i][j] > matrix[points[idx]->y][points[idx]-> x])
+      {
+        points[idx]->x = j;
+        points[idx]->y = i;
+      }
+      for (size_t h = 0; h < height; h++)
+      {
+
+      }
+    }
+  }
+}
+void free_saddle_points(saddle_points_t saddle);
--- c/saddle-points/saddle_points.h
+++ c/saddle-points/saddle_points.h
@@ -1,4 +1,15 @@
 #ifndef SADDLE_POINTS_H
 #define SADDLE_POINTS_H
 
+#include <stdlib.h>
+typedef struct{
+  size_t x;
+  size_t y;
+}saddle_point_t;
+
+saddle_points_t *saddle_points(size_t width, size_t height, uint8_t *matrix);
+saddle_points_t *init_saddle_point(size_t count);
+void free_saddle_points(saddle_points_t saddle);
+
+
 #endif
