a201c97378c8a66bc1da22e919b9e27d4b8d973b
otahiri <chucy0066@gmail.com>
2025-10-08 19:15:39 +0100

    Update python

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,9 @@
 def is_armstrong_number(number):
-    pass
+    tmp = number
+    res = 0
+    power = len(str(number))
+    while tmp > 0:
+        res += pow(tmp % 10, power)
+        tmp //= 10
+    print(res)
+    return number == res
