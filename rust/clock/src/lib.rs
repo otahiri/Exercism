abdbdd6c067e3a2ee818e17d5e2e2442e522e425
otahiri <chucy0066@gmail.com>
2026-02-23 16:26:18 +0000

    added some stuff for the clock exercise in rust

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,27 @@
-pub struct Clock;
+use std::fmt;
+#[derive(PartialEq)]
+pub struct Clock
+{
+    pub hours: i32,
+    pub minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         let temp: i32 = minutes + (hours * 60);
+        let rolled_minute = ((temp % 1440) + 1440) % 1440;
+        let minute = rolled_minute % 60;
+        let hour = rolled_minute / 60;
+        Clock {hours: hour, minutes: minute}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        let mut temp: i32 = (self.minutes * 60) + self.minutes;
+        temp += minutes;
+        let rolled_minute = ((temp % 1440) + 1440) % 1440;
+        let minute = rolled_minute % 60;
+        let hour = rolled_minute / 60;
+        Clock {hours: hour, minutes: minute}
     }
-}
+
+           }
