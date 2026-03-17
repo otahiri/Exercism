ab76a1f78f5a5f3042436ca6e7a654d3d34e20d1
otahiri <chucy0066@gmail.com>
2026-03-17 01:12:52 +0000

    added some stuff to the clock need more work

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,27 +1,12 @@
-use std::fmt;
-#[derive(PartialEq)]
-pub struct Clock
-{
-    pub hours: i32,
-    pub minutes: i32,
-}
+pub struct Clock;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let temp: i32 = minutes + (hours * 60);
-        let rolled_minute = ((temp % 1440) + 1440) % 1440;
-        let minute = rolled_minute % 60;
-        let hour = rolled_minute / 60;
-        Clock {hours: hour, minutes: minute}
+        let total: i32 = (hours * 60) + minutes;
+        total %= 1440;
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut temp: i32 = (self.minutes * 60) + self.minutes;
-        temp += minutes;
-        let rolled_minute = ((temp % 1440) + 1440) % 1440;
-        let minute = rolled_minute % 60;
-        let hour = rolled_minute / 60;
-        Clock {hours: hour, minutes: minute}
+        todo!("Add {minutes} minutes to existing Clock time");
     }
-
-           }
+}
